e160e1df4d19a07e278150d9c1443b2b86bef497
Dimitris Karatzas <dim_kar@outlook.com>
2022-12-21 13:58:55 +0200

    Add project files.

---
+++ ReactMeals_WebApi/Models/Dish.cs
@@ -0,0 +1,10 @@
+﻿namespace ReactMeals_WebApi.Models
+{
+    public class Dish
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
