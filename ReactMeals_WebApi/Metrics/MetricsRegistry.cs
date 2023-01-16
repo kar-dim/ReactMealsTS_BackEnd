23f1ab42de74f7d266801492b4afad8005f1dd7b
Dimitris Karatzas <dim_kar@outlook.com>
2023-01-16 12:29:50 +0200

    basic app metrics expose for orders/get dish(es)
    scrape with prometheus and use in grafana, works!

---
+++ ReactMeals_WebApi/Metrics/MetricsRegistry.cs
@@ -0,0 +1,29 @@
+﻿using App.Metrics;
+using App.Metrics.Counter;
+
+namespace ReactMeals_WebApi.Metrics
+{
+    public class MetricsRegistry
+    {
+        public static CounterOptions CreatedOrdersCounter => new CounterOptions
+        {
+            Name = "Created Orders",
+            Context = "ReactMeals_WebApi",
+            MeasurementUnit = Unit.Calls
+        };
+
+        public static CounterOptions DishRequestCounter => new CounterOptions
+        {
+            Name = "(One) Dish Request",
+            Context = "ReactMeals_WebApi",
+            MeasurementUnit = Unit.Calls
+        };
+
+        public static CounterOptions DishesRequestCounter => new CounterOptions
+        {
+            Name = "Get All Dishes Request",
+            Context = "ReactMeals_WebApi",
+            MeasurementUnit = Unit.Calls
+        };
+    }
+}
