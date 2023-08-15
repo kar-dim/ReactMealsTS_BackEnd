540a33b5f903c906f9b67814cfd5ce789b0b9dcc
Mimis <dim_kar@outlook.com>
2023-08-15 22:19:00 +0300

    Add NGROK service (dev only)

---
+++ ReactMeals_WebApi/Services/NgrokTunnelService.cs
@@ -0,0 +1,62 @@
+﻿using CliWrap;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Hosting.Server;
+using System.Text.Json.Nodes;
+
+namespace ReactMeals_WebApi.Services
+{
+    public class NgrokTunnelService : BackgroundService
+    {
+        private readonly IServer _server;
+        private readonly IHostApplicationLifetime _hostApplicationLifetime;
+        private readonly ILogger<NgrokTunnelService> _logger;
+        private readonly IConfiguration _config;
+
+        public NgrokTunnelService(IConfiguration config, IServer server, IHostApplicationLifetime hostApplicationLifetime, ILogger<NgrokTunnelService> logger)
+        {
+            this._config = config;
+            this._server = server;
+            this._hostApplicationLifetime = hostApplicationLifetime;
+            this._logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await WaitForApplicationStarted();
+
+            var urls = _server.Features.Get<IServerAddressesFeature>()!.Addresses;
+            var localUrl = urls.Single(u => u.StartsWith("http://"));
+            var ngrokDomain = _config["ngrok:url"];
+            _logger.LogInformation("Starting ngrok tunnel for {LocalUrl}", localUrl);
+            var ngrokTask = StartNgrokTunnel(localUrl, ngrokDomain, stoppingToken);
+
+            _logger.LogInformation("Public ngrok URL: {NgrokPublicUrl}", ngrokDomain);
+
+            await ngrokTask;
+
+            _logger.LogInformation("Ngrok tunnel stopped");
+        }
+
+        private Task WaitForApplicationStarted()
+        {
+            var completionSource = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
+            _hostApplicationLifetime.ApplicationStarted.Register(() => completionSource.TrySetResult());
+            return completionSource.Task;
+        }
+
+        private CommandTask<CommandResult> StartNgrokTunnel(string localUrl, string ngrokUrl, CancellationToken stoppingToken)
+        {
+            var ngrokTask = Cli.Wrap("ngrok")
+                .WithArguments(args => args
+                    .Add("http")
+                    .Add("--domain="+ngrokUrl)
+                    .Add(localUrl)
+                    .Add("--log")
+                    .Add("stdout"))
+                .WithStandardOutputPipe(PipeTarget.ToDelegate(s => _logger.LogDebug(s)))
+                .WithStandardErrorPipe(PipeTarget.ToDelegate(s => _logger.LogError(s)))
+                .ExecuteAsync(stoppingToken);
+            return ngrokTask;
+        }
+    }
+}
