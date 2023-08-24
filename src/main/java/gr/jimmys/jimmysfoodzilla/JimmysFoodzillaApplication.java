d2893db08605743b187d85eae3f8e3aa47826a7d
Mimis <dim_kar@outlook.com>
2023-08-24 17:34:54 +0300

    (misc, ignore)

--- src/main/java/gr/jimmys/jimmysfoodzilla/JimmysFoodzillaApplication.java
+++ src/main/java/gr/jimmys/jimmysfoodzilla/JimmysFoodzillaApplication.java
@@ -4,13 +4,11 @@ import gr.jimmys.jimmysfoodzilla.services.JwtValidationAndRenewalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableCaching
 public class JimmysFoodzillaApplication {
 
 	@Autowired
@@ -18,7 +16,6 @@ public class JimmysFoodzillaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JimmysFoodzillaApplication.class, args);
 	}
-
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
